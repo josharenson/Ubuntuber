b25303a44e8ded3c3f8f357b0d91a5790d146704
Josh Arenson <joshua.arenson@canonical.com>
2014-11-18 18:30:49 -0800

    Add api getters for base url and products url

--- src/plugin/qtber_backend/configfile.cpp
+++ src/plugin/qtber_backend/configfile.cpp
@@ -26,7 +26,10 @@ ConfigFile::ConfigFile(QObject *parent) : QObject(parent)
         m_uber_api_authorization_url = QString::fromStdString(uaau);
 
         std::string uabu = cfg.lookup("uber_api_base_url");
-        //m_uber
+        m_uber_api_base_url = QString::fromStdString(uabu);
+
+        std::string uapu = cfg.lookup("uber_api_products_url");
+        m_uber_api_products_url = QString::fromStdString(uapu);
 
         std::string uaoci = cfg.lookup("uber_api_oauth_client_id");
         m_uber_api_oauth_client_id = QString::fromStdString(uaoci);
@@ -39,6 +42,16 @@ ConfigFile::ConfigFile(QObject *parent) : QObject(parent)
 QString ConfigFile::uberApiAuthorizationUrl()
 {
     return m_uber_api_authorization_url;
+}
+
+QString ConfigFile::uberApiBaseUrl()
+{
+    return m_uber_api_base_url;
+}
+
+QString ConfigFile::uberApiProductsUrl()
+{
+    return m_uber_api_products_url;
 }
 
 QString ConfigFile::uberApiOauthClientId()
